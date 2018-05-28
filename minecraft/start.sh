956da7db4771d266fadf8d8a7cf3dc8924e20cfc
Marco Antônio <ma.souza.junior@gmail.com>
2018-05-28 14:25:53 -0300

    FEA : running version 1.12.2

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -1,8 +1,11 @@
+docker stop mc
+docker rm mc
 docker run -d \
   -v $(pwd)/data:/data \
   -e VERSION=1.7.10 \
-  -e TYPE=SPIGOT -e VERSION=1.12 \
+  -e TYPE=SPIGOT -e VERSION=1.12.2 \
   -p 25565:25565 -e EULA=TRUE --name mc itzg/minecraft-server
+docker start mc
 
 # -e TYPE=FORGE -e FORGEVERSION=10.13.4.1448 \
 
